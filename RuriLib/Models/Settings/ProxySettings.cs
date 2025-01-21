bbd69bdce1177546058adebf9b6ddf70874c5c80
ocim128 <41606534+ocim128@users.noreply.github.com>
2025-01-21 11:42:06 +0700

    Update ProxySettings.cs

--- RuriLib/Models/Settings/ProxySettings.cs
+++ RuriLib/Models/Settings/ProxySettings.cs
@@ -5,7 +5,7 @@ namespace RuriLib.Models.Settings
     public class ProxySettings
     {
         public int ProxyConnectTimeoutMilliseconds { get; set; } = 5000;
-        public int ProxyReadWriteTimeoutMilliseconds { get; set; } = 10000;
+        public int ProxyReadWriteTimeoutMilliseconds { get; set; } = 60000;
         public List<string> GlobalBanKeys { get; set; } = new List<string>();
         public List<string> GlobalRetryKeys { get; set; } = new List<string>();
     }
